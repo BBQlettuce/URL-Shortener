12225c74fddccab2025b536e81c4683aa9149330
Oscar+Jason <oscar+jason@appacademy.io>
2015-08-12 16:05:32 -0400

    write ShortenedUrl::random_code

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -3,4 +3,14 @@ class ShortenedUrl < ActiveRecord::Base
   validates :long_url, uniqueness: true
   validates :short_url, presence: true
   validates :short_url, uniqueness: true
+
+  def self.random_code
+    code = SecureRandom.urlsafe_base64
+    until !ShortenedUrl.exists?(:short_url => code)
+      code = SecureRandom.urlsafe_base64
+    end
+    code
+  end
+
+
 end
